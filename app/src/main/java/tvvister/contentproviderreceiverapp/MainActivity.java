6d73c02b597e897e2df37f0a572f308ea78566f8
Andrey <tvvister@mail.ru>
2016-08-09 02:19:21 +0400

    for pr

--- app/src/main/java/tvvister/contentproviderreceiverapp/MainActivity.java
+++ app/src/main/java/tvvister/contentproviderreceiverapp/MainActivity.java
@@ -16,6 +16,9 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.listView) ListView listView;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
